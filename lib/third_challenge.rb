540888f45366986a23e8a0d906202fc029e8f5ac
Emily Wessel <ettl02@gmail.com>
2018-06-24 20:45:54 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,13 +3,13 @@ def third_challenge
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},#your key/value pairs here
       :matriarch => {name: "Lady Montague", age: "54"},
-      :hero => {name: "Romeo", age: "15", status: "alive"},
+      :hero => {name: "Romeo", age: "15", status: "dead"},
       :hero_friends => []
    },
     :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
-      :heroine => {name: "Lady Capulet", age: "51"},
+      :heroine => {name: "Juliet", age: "15", status: "dead"},
       :heroine_friends => []
    }
   }
